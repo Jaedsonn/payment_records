3202ef4665f4a6a918e34e7f8592852af2ce8aa6
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-09-01 23:28:11 -0300

    feat: enhance user registration with password hashing and error handling

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { User } from "@modules/User/entity/user.entity";
-import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { AuthRepository } from "./repository/auth.repository";
+import {hashPassword} from "@lib/utils"
+import { ErrorEnum } from "@lib/enums";
 
 export class AuthService{
   constructor(
@@ -11,9 +12,10 @@ export class AuthService{
   async register(createUserDTO: CreateUserDto): Promise<User> {
     const isAlreadyRegistered = await this.authRepository.findByEmail(createUserDTO.email);
     if (isAlreadyRegistered) {
-      throw new Error('User already registered');
+      throw new Error(ErrorEnum.USER_ALREADY_EXISTS.message);
     }
-    const user = this.authRepository.create(createUserDTO);
+    const user = await  this.authRepository.create(createUserDTO);
+    createUserDTO.password = await hashPassword(createUserDTO.password);
     Object.assign(user, createUserDTO);
     return user;
   }
