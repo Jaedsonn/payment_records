56473affbeec95c9ea79e32154f160809758e3ff
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-09-05 23:05:51 -0300

    feat: add function to extract refresh token from headers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,13 @@ export const hashPassword = async (password: string): Promise<string> => {
 export const comparePasswords = async (password: string, hash: string): Promise<boolean> => {
   return bcrypt.compare(password, hash);
 };
+
+export const extractTokenFromHeader = (headers: any) => {
+  const cookies = headers?.cookie;
+
+  const config = cookies.split(';').filter((cookie: string) => cookie.trim().startsWith('refresh_token='))[0].split('=')[1];
+
+  if(!config) return '';
+
+  return config;
+}
