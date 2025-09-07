5b8f2be8740697a7579ed2c05885d045f943bb7b
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-09-07 14:29:56 -0300

    feat(user): implement UserRepository with CRUD methods for user management

---
+++ src/modules/User/repository/user.repository.ts
@@ -0,0 +1,33 @@
+import { User } from "@modules/User/entity/user.entity"
+import { AppDataSource } from "@shared/data-source";
+import { Repository } from "typeorm";
+
+interface IUserRepository<T> {
+  findByEmail(email: string): Promise<T | null>;
+  findById(id: string): Promise<T | null>;
+  updateUser(id: string, data: Partial<T>): Promise<T>;
+  deleteUser(id: string): Promise<void>;
+}
+
+export class UserRepository implements IUserRepository<User> {
+  constructor(
+    private readonly userRepository: Repository<User> = AppDataSource.getRepository(User)
+  ){}
+
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.userRepository.findOne({where: {email}});
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return await this.userRepository.findOne({where: {id}});
+  }
+
+  async updateUser(id: string, data: Partial<User>): Promise<User> {
+    return await this.userRepository.save({...data, id} as User);
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    return await this.userRepository.delete(id).then(() => {});
+  }
+}
