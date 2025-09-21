f3d88abfe87db35f9d0cde5b8727f27a241897af
Jaedsonn <jaedsonnm@gmail.com>
2025-09-21 17:50:47 -0300

    feat: Add bodyparser middleware to auth routes

--- src/modules/Auth/auth.routes.ts
+++ src/modules/Auth/auth.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { AuthFactory } from "./auth.factory";
+import { bodyParser } from "@middlewares/bodyparser";
+import { CreateUserSchema, ForgotPasswordSchema, LoginUserSchema, ResetPasswordSchema } from "@lib/schema";
 
 const AuthRouter = Router();
 
-AuthRouter.post('/register',  AuthFactory.createAuthController().register);
-AuthRouter.post('/login', AuthFactory.createAuthController().login)
-AuthRouter.post('/refresh-token', AuthFactory.createAuthController().refreshToken)
-AuthRouter.post('/logout', AuthFactory.createAuthController().logout)
-AuthRouter.post('/forgot-password', AuthFactory.createAuthController().forgotPassword)
-AuthRouter.post('/reset-password', AuthFactory.createAuthController().resetPassword)
+AuthRouter.post('/register',bodyParser(CreateUserSchema),  AuthFactory.createAuthController().register);
+AuthRouter.post('/login',bodyParser(LoginUserSchema),  AuthFactory.createAuthController().login);
+AuthRouter.post('/refresh-token',  AuthFactory.createAuthController().refreshToken);
+AuthRouter.post('/logout',  AuthFactory.createAuthController().logout);
+AuthRouter.post('/forgot-password',bodyParser(ForgotPasswordSchema),  AuthFactory.createAuthController().forgotPassword);
+AuthRouter.post('/reset-password',bodyParser(ResetPasswordSchema),  AuthFactory.createAuthController().resetPassword);
 
 export default AuthRouter;
