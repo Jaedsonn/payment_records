5313c022a42abe085899ea8af5950aa59ae8cb04
Jaedsonn <jaedsonnm@gmail.com>
2025-09-21 17:50:11 -0300

    refactor: replace manual error for zod error in bodyparser middleware

--- src/middlewares/bodyparser.ts
+++ src/middlewares/bodyparser.ts
@@ -3,14 +3,18 @@ import { Request, Response, NextFunction } from "express";
 import { ErrorEnum } from "@lib/enums";
 
 export function bodyParser(zodSchema: z.ZodTypeAny) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const parsedBody = zodSchema.parse(req.body);
-            req.body = parsedBody;
-            next();
-        } catch (error) {
-            return res.status(400).json({ message: ErrorEnum.BAD_REQUEST.message, error });
-        }
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const parsedBody = zodSchema.parse(req.body);
+      req.body = parsedBody;
+      next();
+    } catch (e) {
+      const formatZodError = JSON.parse(e.message);
+      return res
+        .status(ErrorEnum.MISSING_PROPERTIES.status)
+        .json({
+          details: formatZodError,
+        });
     }
+  };
 }
-    
