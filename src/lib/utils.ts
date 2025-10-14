00ba0fed963d7ccb05a13ae094ab1204ab7457e2
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-10-14 10:52:00 -0300

    refactor: update extractTokenFromHeader function to use IncomingHttpHeaders type and improve cookie handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import bcrypt from "bcryptjs";
+import { IncomingHttpHeaders } from "http";
 
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
@@ -9,10 +10,16 @@ export const comparePasswords = async (password: string, hash: string): Promise<
   return bcrypt.compare(password, hash);
 };
 
-export const extractTokenFromHeader = (headers: any, tokenType: 'access_token' | 'refresh_token') => {
+export const extractTokenFromHeader = (headers: IncomingHttpHeaders, tokenType: 'access_token' | 'refresh_token') => {
   const cookies = headers?.cookie;
 
-  const config = cookies.split(';').filter((cookie: string) => cookie.trim().startsWith(`${tokenType}=`))[0].split('=')[1];
+  if (!cookies) return '';
+
+  const cookieArray = cookies.split(';').filter((cookie: string) => cookie.trim().startsWith(`${tokenType}=`));
+  
+  if (cookieArray.length === 0) return '';
+
+  const config = cookieArray[0].split('=')[1];
 
   if(!config) return '';
 
