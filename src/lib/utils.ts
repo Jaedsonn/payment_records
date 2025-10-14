e31e2cd4a0b34f3ca0ee43f7f94ee297ec4a3321
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-10-14 10:52:11 -0300

    refactor: update environment variables and enhance utility functions for improved readability and cookie handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,22 +6,30 @@ export const hashPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password, salt);
 };
 
-export const comparePasswords = async (password: string, hash: string): Promise<boolean> => {
+export const comparePasswords = async (
+  password: string,
+  hash: string
+): Promise<boolean> => {
   return bcrypt.compare(password, hash);
 };
 
-export const extractTokenFromHeader = (headers: IncomingHttpHeaders, tokenType: 'access_token' | 'refresh_token') => {
+export const extractTokenFromHeader = (
+  headers: IncomingHttpHeaders,
+  tokenType: "access_token" | "refresh_token"
+) => {
   const cookies = headers?.cookie;
 
-  if (!cookies) return '';
+  if (!cookies) return "";
 
-  const cookieArray = cookies.split(';').filter((cookie: string) => cookie.trim().startsWith(`${tokenType}=`));
-  
-  if (cookieArray.length === 0) return '';
+  const cookieArray = cookies
+    .split(";")
+    .filter((cookie: string) => cookie.trim().startsWith(`${tokenType}=`));
 
-  const config = cookieArray[0].split('=')[1];
+  if (cookieArray.length === 0) return "";
 
-  if(!config) return '';
+  const config = cookieArray[0].split("=")[1];
+
+  if (!config) return "";
 
   return config;
-}
+};
