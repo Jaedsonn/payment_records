da3bfb488c7511e52b94b1219591dd848b9281ec
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-10-23 08:41:21 -0300

    fix: refactor data-source.ts to ensure consistent import style and load environment variables

--- src/shared/db/data-source.ts
+++ src/shared/db/data-source.ts
@@ -1,10 +1,11 @@
-import * as typeorm from "typeorm"
-import { Bank } from '@modules/Bank/entity/bank.entity';
-import { User } from '@modules/User/entity/user.entity';
-import { Transaction } from '@modules/Transaction/entity/trasaction.entity';
+import * as typeorm from "typeorm";
+import { Bank } from "@modules/Bank/entity/bank.entity";
+import { User } from "@modules/User/entity/user.entity";
+import { Transaction } from "@modules/Transaction/entity/trasaction.entity";
 import { Account } from "@modules/Account/entity/account.entity";
+import * as dotenv from "dotenv";
 
-process.loadEnvFile()
+dotenv.config();
 
 export const AppDataSource = new typeorm.DataSource({
   type: "postgres",
@@ -18,4 +19,4 @@ export const AppDataSource = new typeorm.DataSource({
   entities: [User, Bank, Transaction, Account],
   subscribers: [],
   migrations: [],
-})
+});
