ec6563dfeafcb2cd37af61bb251cbd991ead0e8e
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2025-10-27 08:09:33 -0300

    feat: add cors configuration to server and replacing the default process.env to .env only

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,25 @@ import { AccountRouter } from "@modules/Account/account.routes";
 import { TransactionRouter } from "@modules/Transaction/transaction.routes";
 import cors from "cors";
 import { runSeeds } from "@shared/seeds";
+import { env } from "@shared/env";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  })
+);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/auth", AuthRouter);
 app.use("/user", UserRouter);
 app.use("/bank", BankRouter);
