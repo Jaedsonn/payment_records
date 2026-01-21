38a449de8ca06d5dcfe140595ec29eb197bc1493
Jaedsonn <jaedsonnm@gmail.com>
2026-01-20 22:08:43 -0300

    feat: setup swagger docs

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ import { TransactionRouter } from "@modules/Transaction/transaction.routes";
 import cors from "cors";
 import { runSeeds } from "@shared/seeds";
 import { env } from "@shared/env";
+import swaggerUi from "swagger-ui-express"
+import swaggerDoc from "swagger-jsdoc"
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -26,6 +28,38 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
+
+const swaggerOptions = { 
+  
+   definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Payment Records API",
+      version: "0.1.0",
+      description:
+        "API for managing payment records, including users, banks, accounts, and transactions.",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "Payment Records Team",
+        url: "https://paymentrecords.com",
+        email: "jaedsonnm@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./src/modules/**/*.ts"],
+}
+
+const swagger = swaggerDoc(swaggerOptions);
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/auth", AuthRouter);
@@ -33,11 +67,13 @@ app.use("/user", UserRouter);
 app.use("/bank", BankRouter);
 app.use("/account", AccountRouter);
 app.use("/transaction", TransactionRouter);
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swagger));
 app.use(ErrorHandler.handle.bind(ErrorHandler));
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ status: "OK" });
 });
+
 
 AppDataSource.initialize()
   .then(() => {
