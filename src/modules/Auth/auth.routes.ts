cea6455051dd83b99673827008a7ab6b27ca437b
Jaedsonn <jaedsonnm@gmail.com>
2026-01-22 22:57:23 -0300

    feat: document user routes

--- src/modules/Auth/auth.routes.ts
+++ src/modules/Auth/auth.routes.ts
@@ -5,6 +5,57 @@ import { CreateUserSchema, ForgotPasswordSchema, LoginUserSchema, ResetPasswordS
 
 const AuthRouter = Router();
 
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateUserDto'
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 user:
+ *                   type: object
+    *                   properties:
+    *                      id:
+    *                       type: string
+    *                      email:
+    *                       type: string 
+    *                      name:
+    *                        type: string
+    *                      password:
+    *                        type: string
+    *                      Accounts:
+    *                        type: array     
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: User already exists
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ */
 AuthRouter.post('/register',bodyParser(CreateUserSchema),  AuthFactory.createAuthController().register);
 AuthRouter.post('/login',bodyParser(LoginUserSchema),  AuthFactory.createAuthController().login);
 AuthRouter.post('/refresh-token',  AuthFactory.createAuthController().refreshToken);
--- src/modules/User/user.routes.ts
+++ src/modules/User/user.routes.ts
@@ -3,8 +3,15 @@ import { UserFactory } from "./user.factory";
 import { validateToken } from "@middlewares/jwt";
 import { bodyParser } from "@middlewares/bodyparser";
 import { UpdateUserSchema } from "@lib/schema";
+import { OpenAPIRegistry  } from '@asteasolutions/zod-to-openapi';
+import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
+import { z } from 'zod';
+
+extendZodWithOpenApi(z);
 
 export const UserRouter = Router();
+export const userRegistry = new OpenAPIRegistry();
+
 
 UserRouter.put(
   "/update",
@@ -13,8 +20,59 @@ UserRouter.put(
    UserFactory.createUserController().updateUser
   );
 
+  userRegistry.registerPath({
+    method: "put",
+    path: "/update",
+    summary: "Update user information",
+    tags: ["User"],
+    request: {
+      body: {
+        content: {
+          "application/json": {
+            schema: UpdateUserSchema
+          }
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: "User updated successfully"
+      },
+      400: {
+        description: "Bad Request",
+        summary: "Return a validation error if the request body does not conform to the UpdateUserSchema"
+      }
+    }
+  })
+
+
 UserRouter.get(
   "/info",
    validateToken,
    UserFactory.createUserController().getUserInfo
   );
+
+userRegistry.registerPath({
+  method: "get",
+  path: "/info",
+  summary: "Get user information",
+  tags: ["User"],
+  responses: {
+    200: {
+      description: "User retrieved successfully",
+      content: {
+        "application/json": {
+          schema: z.object({
+            success: z.boolean().openapi({example: true}),
+            message: z.string().openapi({example: "User retrieved successfully"}),
+            data: UpdateUserSchema
+          })
+        }
+      }
+    },
+    404: {
+      description: "Not Found",
+      summary: "Return an error if the user is not found"
+    }
+  }
+})
